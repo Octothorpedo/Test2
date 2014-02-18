cf2852ad4409e37642459ac22a9753bc574d4234
Crystal08 <Crystal08@compuserve.com>
2014-02-18 11:18:34 -0600

    Testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Test2::Application.routes.draw do
   resources :articles
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
